1111900048aa96666770e6a795b1497d2356d80b
vqstark <vuquan12112001@gmail.com>
2023-04-21 13:55:08 +0700

    Add bottom navigation bar, modify HomeActivity

---
+++ app/src/main/java/com/example/gon17/activity/home/fragment/AccountFragment.java
@@ -0,0 +1,24 @@
+package com.example.gon17.activity.home.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gon17.R;
+
+public class AccountFragment extends Fragment {
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+}
